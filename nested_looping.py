8fd624224e6c9e984216f72b833307010363e4d3
Roberto Santiago <134346516+RobertodSilvaSantiago@users.noreply.github.com>
2023-06-11 02:26:34 +0100

    Update nested_looping.py

--- nested_looping.py
+++ nested_looping.py
@@ -1,37 +1,53 @@
-def print_triangle(n):
-    if n == -1:
-        print("Bye!")
-        return False
-
-    command = input("Please enter command (triangle/mp): ")
-
-    if command == "mp":
-        for i in range(1, n + 1):
-            row = [i * j for j in range(1, n + 1)]
-            print(" ".join(f"{num:2d}" for num in row))
-    elif command == "triangle":
-        for i in range(1, n + 1):
-            row = [str(j) for j in range(1, i + 1)]
-            print(" ".join(row))
-        for i in range(n - 1, 0, -1):
-            row = [str(j) for j in range(1, i + 1)]
-            print(" ".join(row))
-    else:
-        print("Invalid command")
-
-    return True
+def print_triangle_pattern(n):
+    for i in range(1, n + 1):
+        row = [str(j) for j in range(1, i + 1)]
+        print(" ".join(row))
+    for i in range(n - 1, 0, -1):
+        row = [str(j) for j in range(1, i + 1)]
+        print(" ".join(row))
 
 
-print("Enter -1 to exit.")
-max_prompts = 5  # Maximum number of prompts before exiting
-num_prompts = 0  # Counter for the number of prompts
+def print_multiplication_table(n):
+    for i in range(1, n + 1):
+        row = [i * j for j in range(1, n + 1)]
+        print(" ".join(f"{num:2d}" for num in row))
 
-while num_prompts < max_prompts:
-    num = int(input("Please enter a number: "))
-    num_prompts += 1
-    if not print_triangle(num):
-        break
 
-    if num_prompts == max_prompts:
-        print("Maximum number of prompts reached. Exiting.")
-        break
+def get_valid_number_input(prompt):
+    while True:
+        try:
+            num = int(input(prompt))
+            return num
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+
+def main():
+    print("Enter -1 to exit.")
+    max_prompts = 5  # Maximum number of prompts before exiting
+    num_prompts = 0  # Counter for the number of prompts
+
+    while num_prompts < max_prompts:
+        num = get_valid_number_input("Please enter a number: ")
+        num_prompts += 1
+
+        if num == -1:
+            print("Bye!")
+            break
+
+        command = input("Please enter command (triangle/mp): ")
+
+        if command == "triangle":
+            print_triangle_pattern(num)
+        elif command == "mp":
+            print_multiplication_table(num)
+        else:
+            print("Invalid command")
+
+        if num_prompts == max_prompts:
+            print("Maximum number of prompts reached. Exiting.")
+            break
+
+
+if __name__ == "__main__":
+    main()
